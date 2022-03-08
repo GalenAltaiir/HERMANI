caf83638fe388c1dc2c8db6d100dc05d62d99dcb
GalenAltaiir <damianglen98@gmail.com>
2022-03-08 15:25:02 +0000

    added category specific displays, and more detailed json

---
+++ js/categories.js
@@ -0,0 +1,54 @@
+
+                // Get url parameters and store them in an array
+                let values = []
+                const params = new URLSearchParams(window.location.search)
+                for (const param of params) {
+                    values.push(param)
+                }
+                
+                const api = "http://localhost/cockpit/api/collections/get/"+values[0][1]+"Categories"
+
+                async function getApi(){
+            /*
+                Asynchronous function which purpose is to fetch
+                the JSON file from the Server (Cockpit CMS) and 
+                display it's content dynamically.
+
+                First we set shorthands and set up the fetch for API,
+                then run it through a for loop to generate HTML elements
+                for each entry.
+
+                All of this is done through JavaScript with the exception
+                of the JSON being stored on Cockpit which is a PHP based
+                CMS. This means that it can be hosted on a static web host,
+                so long the CMS is running elsewhere.
+
+                TO DO:
+                - LIMIT ENTRIES PER PAGE (50 maybe)
+            */
+            const response = await fetch(api)
+            const data = await response.json()
+            const {entries} = data
+            
+            let container = document.getElementById("men-subcat");
+            let countEntries = entries.length
+            
+            for(let i = 0; i <= countEntries; i++){
+                let entry = entries[i] // refers to the first entry in the JSON
+                // delete above 1 line
+                let type = entries[i].type.toUpperCase();
+                let url = entries[i].type.toLowerCase();
+                let img = entries[i].img.path
+                container.innerHTML+=
+                // http://localhost/cockpit/api/collections/get/MenClothing?filter[$and][][category]=Men&filter[$and][][type]=Bag
+                    `
+                        <div class="product-type">
+                        <a class="prod-name" href=../search.html?category=`+values[0][1]+`&type=`+url+`>`+type+`</a>
+                        <img src="../`+img+`" alt="">
+                        </div>
+                    `
+                    
+                }
+    }
+
+        getApi();
--- js/search.js
+++ js/search.js
@@ -1,6 +1,18 @@
-                const api = "http://localhost/cockpit/api/collections/get/MenClothing"
-                let urlRaw = window.location.href;
-                console.log(urlRaw)
+                let values = []
+                const params = new URLSearchParams(window.location.search)
+                for (const param of params) {
+                    values.push(param)
+                }
+
+                let category = values[0][1]
+                let type = values[1][1]
+
+                console.log(category)
+                
+                const api = "http://localhost/cockpit/api/collections/get/"+category+"Clothing?filter[$and][][category]="+category+"&filter[$and][][type]="+type
+                console.log(api)
+                // MULTI FILTER EXAMPLE
+
         async function getApi(){
             /*
                 Asynchronous function which purpose is to fetch
@@ -22,42 +34,42 @@
             const response = await fetch(api)
             const data = await response.json()
             const {entries} = data
-            let productName = document.getElementsByClassName("prod-name")
-            let productBrand = document.getElementsByClassName("prod-brand")
-            let productPrice = document.getElementsByClassName("prod-price")
-            let productImg = document.getElementsByClassName("prod-img")
             
             let container = document.getElementById("products");
             let countEntries = entries.length
+
+            let head = document.getElementById("title")
+            let capCat = category[0].toUpperCase() + category.substring(1)
+            let capType = type[0].toUpperCase() + type.substring(1)
+            let title = capCat + "'s Designer " + capType;
+            head.innerHTML = title
+
             
-            for(let i = 0; i <= countEntries - 1; i++){
+            for(let i = 0; i <= countEntries; i++){
                 let entry = entries[i] // refers to the first entry in the JSON
                 
-                container.innerHTML+=
-                `
-                <div class="product-type">
-                <img class = "prod-img" src="" alt="">
-                <a class="heart" href='#'><i id="heart" class="fa-solid fa-heart"></i></a>
-                <div class="prod-name"></div><div class="prod-brand">
-                </div>
-                <div class="prod-price"></div>
-                </div>
-                `
                 
-                // the index here refers to the HTML block classes (first being 0)
-                productName[i].classList.add("prod-name")
-                productName[i].innerHTML += entry.itemName
-
-                // display name and center it
-                productBrand[i].innerHTML = entry.itemBrand
-
-                // display brand and center it
-                productPrice[i].innerHTML = '£'+entry.itemPrice
-
-                // display price and center it
-                productImg[i].src = entry.itemImage.path
-                // places product image
-        }
+                container.innerHTML+=
+                    `
+                    <div class="product-type">
+                    <img class = "prod-img" src="`+entry.itemImage.path+`" alt="">
+                    
+                    <a class="heart" href='#'><i id="heart" class="fa-solid fa-heart"></i></a>
+                    
+                    <div class="prod-name">
+                    `+entry.itemName+ /* data insertion */ `
+                    </div>
+                    <div class="prod-brand">
+                    `+entry.itemBrand+`
+                    </div>
+                    <div class="prod-price">
+                    £`+entry.itemPrice+`
+                    </div>
+                    
+                    </div>
+                    `
+                    
+                }
     }
 
         getApi();
